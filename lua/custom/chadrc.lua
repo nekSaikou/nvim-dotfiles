543127dba024e19b0632ab11781dd83f33449a65
saikou <neko.saikou@tutanota.com>
2024-02-22 16:32:37 +0700

    lsp

---
+++ lua/custom/chadrc.lua
@@ -0,0 +1,9 @@
+---@type ChadrcConfig
+local M = {}
+
+M.ui = {
+  theme = 'ayu_dark'
+}
+M.plugins = "custom.plugins"
+
+return M
---
+++ lua/custom/configs/lspconfig.lua
@@ -0,0 +1,22 @@
+local configs = require("plugins.configs.lspconfig")
+local on_attach = configs.on_attach
+local capabilities = configs.capabilities
+
+local lspconfig = require "lspconfig"
+local servers = {
+  "bashls",
+  "csharp_ls",
+  "clangd",
+  "cmake",
+  "hls",
+  "zls",
+  "pylsp",
+  "rust_analyzer"
+}
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
